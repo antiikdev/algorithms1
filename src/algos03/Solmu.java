0da32f7e45aa0d32abf824aaec2d914421cb427d
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-04 20:02:39 +0800

    Start of binary tree O

---
+++ src/algos03/Solmu.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package algos03;
+
+/**
+ * @author AntiikDev
+ * @ver 4 Feb 2022
+ */
+public class Solmu {
+	
+	public char key;
+	public Solmu left;
+	public Solmu right;
+
+	
+	
+	/**
+	 * Main for testing
+	 * @param args not in use
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
