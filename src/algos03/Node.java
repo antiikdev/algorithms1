714724e3a6165caf9d9f7244b7ad5108ca0a6819
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-06 21:50:55 +0800

    Node started

---
+++ src/algos03/Node.java
@@ -0,0 +1,29 @@
+/**
+ * Binary tree nodes (Finn. solmut) in a) orders
+ * as well as a) and b) search with variable pointers
+ */
+package algos03;
+
+/**
+ * @author AntiikDev
+ * @ver 6 Feb 2022
+ */
+public class Node {
+
+					 //     0   1   2   3   4   5   6   7   8   9
+	static int[] array = { 30, 20, 37 , 15, 23, 32, 40, 16, 31, 33};
+	
+	// Nodes (Finn. solmut)
+	static Node thirty = new Node(array[0]);
+
+
+	/**
+	 * Main for testing
+	 * @param args not in use
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
