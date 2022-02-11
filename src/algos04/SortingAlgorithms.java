5b9331bc9818857f126f688f47e02294eb60febb
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-11 16:54:15 +0800

    bubble sort

--- src/algos04/SortingAlgorithms.java
+++ src/algos04/SortingAlgorithms.java
@@ -1,5 +1,5 @@
 /**
- * Bubble sort and ... algorithms
+ * Bubble and insertion sort algorithms
  */
 package algos04;
 
@@ -13,11 +13,40 @@ public class SortingAlgorithms {
 	
 	
 	/**
-	 * Bubble sort for integer numbers
+	 * Bubble sort for integer number array
 	 * @param array to be sorted
 	 * @return sorted array
 	 */
 	public static int[] BubbleSort(int[] array) {
+		int temp = 0;
+		for (int i = 1; i < array.length; i++) {
+			for (int j = array.length-2; j >=0 ; j--) {
+				if (array[j] < array[j+1]) {
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
+		return array;
+	}
+	
+	
+	/**
+	 * Insertion sort with integer array
+	 * @param array to be sorted
+	 * @return insertion sorted array
+	 */
+	public static int[] insertionSort(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			int p = array[i];
+			int j = i-1;
+			while (j >= 0 && array[i] > p) {
+				array[j+1] = array[j];
+				j = j-1;
+			}
+			array[j+1] = p;
+		}
 		return array;
 	}
 	
@@ -28,17 +57,20 @@ public class SortingAlgorithms {
 	 */
 	public static void main(String[] args) {
 		int[] array = null;
-		array = algos01.RandomArrayO1.addRandomNumbers(array, 0, 10);
-		System.out.println(Arrays.toString(array));
+		array = algos01.RandomArrayO1.addRandomNumbers(array, 1, 10);
+		System.out.println("Random array: " + Arrays.toString(array));
 		
 		// Bubble sort
 		array = BubbleSort(array);
-		System.out.println(Arrays.toString(array));
+		System.out.println("Bubble sort:  " + Arrays.toString(array));
 		
-		// TODO: sort
-		array = algos01.RandomArrayO1.addRandomNumbers(array, 0, 10);
-		System.out.println(Arrays.toString(array));
-		
+		// TODO: Insertion sort
+		System.out.println("------");
+		int[] array2 = null;
+		array2 = algos01.RandomArrayO1.addRandomNumbers(array2, 1, 10);
+		System.out.println("Random array:   " + Arrays.toString(array2));
+		array2 = insertionSort(array2);
+		System.out.println("Insertion sort: " + Arrays.toString(array2));
 	}
 
 }
