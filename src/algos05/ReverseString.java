a463e051d2f2200a8a03fcf19debebe8b72e94e5
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-18 23:11:57 +0800

    cut individual words

--- src/algos05/ReverseString.java
+++ src/algos05/ReverseString.java
@@ -3,6 +3,8 @@
  */
 package algos05;
 
+import java.util.Arrays;
+
 /**
  * @author AntiikDeb
  * @version 17 Feb 2022
@@ -16,11 +18,34 @@ public class ReverseString {
 	 * @return reversed string
 	 */
 	public static String reverse(String jono) {
+		if (jono.length() <= 0) return jono;
+				
 		// to char array
-		char[] array = stringToChar(jono);
+		char[] charArray = stringToChar(jono);
+		System.out.println(Arrays.toString(charArray)); // TEST REMOVE
 		
-		// cut individual words, create Strings and add to a String array
+		// cut individual words
+		char[] yksiSana = new char[jono.length()];
+		String[] jonotaulu = new String[jono.length()]; // extra long array
 		
+		int i = 0, j = 0;
+		while(i < charArray.length) {
+			char kirjain = charArray[i];
+			yksiSana[i] = kirjain;
+			if ( kirjain == ' ' ) {
+				String sana = String.valueOf(yksiSana);
+				System.out.println(sana); // TEST REMOVE
+				jonotaulu[j] = sana;
+				j++;
+			}
+			i++;
+		}
+		
+		System.out.println(Arrays.toString(jonotaulu)); // TEST REMOVE
+		
+		// create Strings and add to a String array
+		
+
 		// reverse String array words
 		
 		// translate to String and retunr
