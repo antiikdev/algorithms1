f91a40f77eb9f19e924ef3c058dd7d809ccf233a
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-19 17:17:44 +0800

    reverse method review

--- src/algos05/ReverseString.java
+++ src/algos05/ReverseString.java
@@ -3,13 +3,18 @@
  */
 package algos05;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author AntiikDeb
  * @version 17 Feb 2022
  */
 public class ReverseString {
+	
+	// String array to save individual words
+	public static List<String> stringArray = new ArrayList<>();
 	
 	
 	/**
@@ -20,31 +25,13 @@ public class ReverseString {
 	public static String reverse(String jono) {
 		if (jono.length() <= 0) return jono;
 				
-		// to char array
+		// String to char array
 		char[] charArray = stringToChar(jono);
-		System.out.println(Arrays.toString(charArray)); // TEST REMOVE
 		
-		// cut individual words
-		char[] yksiSana = new char[jono.length()];
-		String[] jonotaulu = new String[jono.length()]; // extra long array
-		
-		int i = 0, j = 0;
-		while(i < charArray.length) {
-			char kirjain = charArray[i];
-			yksiSana[i] = kirjain;
-			if ( kirjain == ' ' ) {
-				String sana = String.valueOf(yksiSana);
-				sana = trim(sana); // Trim the word
-				System.out.println(sana); // TEST REMOVE
-				jonotaulu[j] = sana;
-				yksiSana = new char[jono.length()];
-				j++;
-				
-			}
-			i++;
-		}
-		
-		System.out.println(Arrays.toString(jonotaulu)); // TEST REMOVE
+		// cut individual words and add to the array
+		int firstIndex = nextCharIndex(charArray);
+		String word = getWord(charArray, firstIndex);
+		stringArray.add(word);
 		
 		// create Strings and add to a String array
 		
@@ -59,29 +46,58 @@ public class ReverseString {
 	
 	
 	/**
-	 * Trims a word by removing empty spaces
-	 * @param jono that is trimmed
-	 * @return trimmed String
+	 * Next index where char
+	 * @param array char
+	 * @return index of next char
 	 */
-	public static String trim(String jono) {
-		if (jono.length() < 0) return jono;
-		char[] charArray = stringToChar(jono);
-		char[] newArray = new char[jono.length()];
-		
-		// TODO: index of ' ' empty spaces
-		
-		for (int i = 0; i < charArray.length; i++) {
-			if ( charArray[i] != ' ' ) {
-				newArray[i] = charArray[i];
-				if ( charArray[i+1] == ' ' || i > charArray.length ) break;
-			}
+	public static int nextCharIndex(char[] array) {
+		int index = 0;
+		for (int i = 0; i < array.length; i++) {
+			char c = array[i];
+			if ( c != ' ' ) return index = i;
 		}
-		String sana = String.valueOf(newArray);
-		System.out.println(sana); // TEST REMOVE
-		return sana;
+		return index;
 	}
 	
 	
+	/**
+	 * 
+	 * @param array
+	 * @param firstIndex
+	 * @return
+	 */
+	public static String getWord(char[] array, int startIndex) {
+		String word = "";
+		for (int i = startIndex; i < array.length; i++) {
+			char c = array[i];
+			if ( c == ' ' ) break;
+			word += c;
+		}
+		return word;
+	}
+		
+	
+	
+	
+	/**
+	 * Trims a word by removing empty spaces
+	 * @param taulu a char array that is trimmed
+	 * @return trimmed String
+	 */
+	public static String trim(char[] taulu) {
+		String sana = "";
+		if (taulu.length < 0) return sana;
+		
+		for (int i=0; i<taulu.length; i++) {
+			char c = taulu[i];
+			if ( c != ' ' ) {
+				sana += c;
+			}
+		}
+		return sana;
+	}
+	
+		
 	/**
 	 * This function prints String to char
 	 * @param array String printed to char
@@ -103,7 +119,7 @@ public class ReverseString {
 	public static void main(String[] args) {
 		String jono = "algoritmien opiskelu on kivaa";
 		System.out.println("Before reverse: " + jono);
-		reverse(jono);
+		jono = reverse(jono);
 		System.out.println("After reverse:  " + jono);
 
 	}
