8d7a4d3adf9392b39cee6619e39f7a552ace11d7
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-22 09:22:36 +0800

    Jono: count longest letter group function

--- src/algos05/Jono.java
+++ src/algos05/Jono.java
@@ -8,6 +8,39 @@ package algos05;
  * @version 21 Feb 2022
  */
 public class Jono {
+	
+	
+	/**
+	 * Counts length of the longest letter group
+	 * @param s String which letters are counted
+	 * @return length int of longest letter group
+	 * @example
+	 * <pre name="test">
+	 * 	String s = "a";
+	 * 	countLongestLetterGroup(s)	=== 1;
+	 * 	String s2 = "aaaabcd";
+	 * 	countLongestLetterGroup(s2)	=== 4;
+	 * 	String s3 = "axboo";
+	 * 	countLongestLetterGroup(s3)	=== 2;
+	 * </pre>
+	 */
+	public static int countLongestLetterGroup(String s) {
+		if ( s.length() < 0 ) return 0;
+		if ( s.length() == 1 ) return 1;
+		char[] ca = stringToChar(s);
+		int longest = 1, current = 1;
+		char previous = ca[0];
+		
+		for (int i = 1; i < ca.length; i++) {
+			if (previous != ca[i]) current = 1;
+			if ( previous == ca[i] ) {
+				current++;
+				if (current > longest) longest = current;
+			}
+			previous = ca[i];
+		}
+		return longest;
+	}
 
 	
 	/**
@@ -78,16 +111,21 @@ public class Jono {
 	 * @param args not in use
 	 */
 	public static void main(String[] args) {
-		// How many a letters
+		// How many 'a' letters in a String
 		String m = "aataminomena";
 		char letter = 'a';
 		int kirjaimet = countLetters(m, letter);
 		System.out.println("String '"+m+"' has "+kirjaimet+" '"+letter+"' letters.");
 		
-		// How many groups of letters
+		// How many groups of letters in a String
 		String m2 = "aabcccaa";
 		int tulos = countGroupsOfLetters(m2);
 		System.out.println("String '"+m2+"' has "+tulos+" groups of letters.");
+		
+		// How long is the longest letter group in a String
+		String m3 = "aabcccaa";
+		int length = countLongestLetterGroup(m3);
+		System.out.println("String '"+m3+"' longest letter group length is "+length);
 	}
 
 }
---
+++ src/algos05/LucasNumber.java
@@ -0,0 +1,23 @@
+/**
+ * Lucas Number: Ln = L(n-1) + L(n-2), when n > 1
+ * and where L1 = 2 and L2 = 1, for example:
+ * Ln = 2, 1, 3, 4, 7, 11, 18, ... 
+ */
+package algos05;
+
+/**
+ * @author AntiikDev
+ * @version 22 Feb 2022
+ */
+public class LucasNumber {
+
+	/**
+	 * Main for testing
+	 * @param args not in use
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
