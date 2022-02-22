a381b65e04d3aa4ac11e2a4f3a46449dee69772e
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-02-22 10:54:50 +0800

    Counting sort function fix

--- src/algos05/Counting_sort.java
+++ src/algos05/Counting_sort.java
@@ -19,7 +19,7 @@ public class Counting_sort {
 	 */
 	public static int findMax(int[] t) {
 		int max = t[0];
-		for (int i = 0; i < t.length; i++) {
+		for (int i = 1; i < t.length; i++) {
 			if (t[i] > max) max = t[i];
 		}
 		return max;
@@ -29,14 +29,15 @@ public class Counting_sort {
 	 * Counting sort (laskentalajittelu)
 	 * Algorighm source: Jyu, algoritmit 1 material
 	 * @param t integer array
-	 * @param max integer value in the table
+	 * @param n integer value in the table
 	 */
-	public static void countingSort(int[] t, int max) {
-		int n = t.length;
-		int[] s = new int[n]; // output
+	public static void countingSort(int[] t, int n) {
+		int max = findMax(t);
+		int[] s = new int[n+1]; // output
 		
 		int[] u = new int[max+1]; // store count (tyotila)
-		for (int i = 0; i < max; i++) {
+		
+		for (int i = 0; i <= max; i++) {
 			u[i] = 0;
 		}
 			// store count zeroed (tyotilataulukko u nollattu)
@@ -45,17 +46,17 @@ public class Counting_sort {
 		}
 			// u[i] includes i count (sisaltaa alkioiden i lukumaaran)
 		for (int i = 1; i <= max; i++) {
-			u[i] = u[i] + u[i-1];
+			u[i] += u[i-1];
 		}
 			// u[i] includes information on how many smaller or bigger
 			// (u[i] sisaltaa tiedon montako alkiota pienempia tai
 			// yhtasuuria kuin i)
-		for (int j = n -1; j >= 0; j--) {
+		for (int j = n - 1; j >= 0; j--) {
 			s[u[t[j]] - 1] = t[j]; // variable t[j] in place (paikalleen)
 			u[t[j]]--; // update count (lukumaaran paivitys)
 		}
 		for (int i = 0; i < n; i++) {
-			t[i] = u[i];
+			t[i] = s[i];
 		}
 		// copy sorted to original array (kopioidaan sortatut
 		// alkuperaiseen taulukkoon)
@@ -69,8 +70,7 @@ public class Counting_sort {
 	public static void main(String[] args) {
 		int[] t = { 6, 3, 6, 1, 9, 4, 6, 8, 3 };
 		System.out.println("Before: " + Arrays.toString(t));
-		int max = findMax(t);
-		countingSort(t, max);
+		countingSort(t, t.length);
 		System.out.println("After:  " + Arrays.toString(t));
 	}
 
